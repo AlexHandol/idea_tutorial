1cac07ef393bde004e62fdbaf3dc1064a0ee9157
AlexHandol <132612905+AlexHandol@users.noreply.github.com>
2024-05-15 10:40:02 +0400

    registration

--- resources/views/includes/idea-card.blade.php
+++ resources/views/includes/idea-card.blade.php
@@ -28,9 +28,7 @@
                 <div class="mb-3">
                     <textarea name="idea-content" class="form-control" id="idea-content" rows="3">{{ $idea->content }}</textarea>
                     @error('idea-content')
-                        <span class="d-block fs-6 text-danger mt-2">
-                            {{ $message }}
-                        </span>
+                        <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -30,6 +31,10 @@ Route::put('/ideas/view/{idea}', [IdeaController::class, 'update'])->name('ideas
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::post('/ideas/comments/{idea}', [CommentController::class, 'store'])->name('ideas.comments.store');
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/register', [AuthController::class, 'store']);
 
 // PAGE NAVIGATIONS
 Route::get('/terms', [PageNavigationController::class, 'termsNav']);
